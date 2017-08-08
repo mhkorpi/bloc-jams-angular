6af8b67e8480a33f42e5e262823ff080c1cb76a5
Michael Korpi <mhkorpi@gmail.com>
2017-08-08 00:28:27 -0700

    Assignment 5 complete: created album controller to fill album info on album page

---
+++ app/scripts/controllers/AlbumCtrl.js
@@ -0,0 +1,25 @@
+(function() {
+	function AlbumCtrl() {
+		this.albumData = angular.copy(albumPicasso);
+		this.songs = [];
+		// create array of song objects for simpler access in HTML
+		for (var i = 0; i < this.albumData.songs.length; i++) {
+			this.songs.push(this.albumData.songs[i]);
+		}
+		this.filterTimeCode = function(timeInSeconds) {
+    		var time = parseFloat(timeInSeconds);
+    		var minutes = Math.floor(time / 60);
+    		var seconds = Math.round(time - minutes * 60);
+
+    		if (seconds < 10) {
+        		return '0:0' + seconds;
+    		} else {
+        		return parseFloat(minutes) + ':' + parseFloat(seconds);
+    		}
+		};
+	}
+
+	angular
+		.module('blocJams') 
+		.controller('AlbumCtrl', AlbumCtrl);
+})();
