48d3e3c553b86ce7a89769f0a3f7d846ff67a701
Michael Korpi <mhkorpi@gmail.com>
2017-08-14 13:55:11 -0700

    Checkpoint 10 complete: seek bar now tracks progress of currently playing song

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -2,18 +2,6 @@
 	function AlbumCtrl(Fixtures, SongPlayer) {
 		this.albumData = Fixtures.getAlbum();
 		this.songPlayer = SongPlayer;
-
-		this.filterTimeCode = function(timeInSeconds) {
-    		var time = parseFloat(timeInSeconds);
-    		var minutes = Math.floor(time / 60);
-    		var seconds = Math.round(time - minutes * 60);
-
-    		if (seconds < 10) {
-        		return '0:0' + seconds;
-    		} else {
-        		return parseFloat(minutes) + ':' + parseFloat(seconds);
-    		}
-		};
 	}
 
 	angular
